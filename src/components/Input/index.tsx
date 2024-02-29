b4371029b7ca0c6c42d21cf6387e669ed69c8864
Maria Gruber <grubermar1992@gmail.com>
2024-02-29 05:54:59 +0100

    bugs fixed

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -21,22 +21,22 @@ const Input: React.FC<InputProps> = ({
   style,
   error
 }) => {
-    const inputClass = classnames('input-form', {
-      'error': Boolean(error)
-    });
-    return (
-      <div className="input-container">
-        <label htmlFor="value">{title}</label>
-        <input
-          className={inputClass}
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          style={style}
-          onChange={(e) => onChange(e.target.value)}
-        />
-      </div>
-    );
-  };
+  const inputClass = classnames('input-form', {
+    'error': Boolean(error)
+  });
+  return (
+    <div className="input-container">
+      <label htmlFor="value">{title}</label>
+      <input
+        className={inputClass}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        style={style}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+};
   
 export {Input};
